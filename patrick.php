5c5791d71b654f9358ad40e6ecb2da1d157a6e73
Goodnew <patricklaedet@gmail.com>
2018-04-24 17:22:51 +0100

    Add patrick.php

---
+++ patrick.php
@@ -0,0 +1,29 @@
+<!DoCTYPE html>
+<html lang="en" >
+    <head>
+ 
+<style>
+h1{font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+color:darkcyan;
+
+font-size: 250%;
+padding-top: 80px; 
+}
+div {padding-top: 150px;
+font-size: 400%;
+color: darkcyan;
+text-align: center;
+}
+</style>
+    <title>HOTEL.NG INTERNSHIP</title>
+    </head>
+<body>
+    <h1 align="center">HOTEL.NG INTERNSHIP 4.0</h1>
+    <div>
+      <?php date_default_timezone_set("Africa/Lagos");?>
+      <?php echo date ("h:i:sa"); ?>
+      
+    </div>
+</body>
+</html>
+
